2209ea43dc97e0837787202459555bb5105e8540
qiheizhiya <953136447@qq.com>
2020-11-03 22:01:07 +0800

    vue3.0-study

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3.0 + Vite" />
+    <div @click="increment">{{count}}</div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import { ref, watchEffect, watch, reactive } from 'vue'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  setup (props, context) {
+    const userInfo = reactive({ name: '杰杰', age: 18 })
+    return { userInfo }
+  },
+  // 等同于
+  data () {
+    return {
+      userInfo: { name: '杰杰', age: 18 }
+    }
+  }
+}
+</script>
